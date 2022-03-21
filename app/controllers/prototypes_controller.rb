03466218ded801584e120dcb4a2421b8c0a80de2
takashi-ogaya <takashi.ogaya@gmail.com>
2022-03-21 14:57:43 +0900

    3/21 14時　修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,8 @@
 class PrototypesController < ApplicationController
-
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update]
-  
+
   def index  # indexアクションを定義した
     @prototypes = Prototype.all
   end
@@ -52,8 +52,14 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image,).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototype.user
   end
+
+
   
 end 
